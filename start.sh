f8747ab6f3c66968c91a0034450f4c435afe61ca
Raflisepuh <148638275+Raflisepuh@users.noreply.github.com>
2023-10-23 00:45:34 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/Raflisepuh/proxy ok && cd ok && pip3 install -U -r requirements.txt
+git clone https://github.com/Raflisepuh/proxy ok && cd ok && pip3 install -r requirements.txt
 python3 bot.py
