b1defdf39bd2be77821cb7f2245b0fab703ab8c0
Raflisepuh <148638275+Raflisepuh@users.noreply.github.com>
2023-10-23 03:57:36 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,1 @@
-git clone $REPO XYZ
-cd XYZ
-pip3 install --upgrade pip
-pip3 install -U -r requirements.txt
-$START_CMD
+git clone https://github.com/Raflisepuh/proxy XYZ; cd XYZ; pip3 install --upgrade pip; pip3 install -U -r requirements.txt; python3 bot.py
